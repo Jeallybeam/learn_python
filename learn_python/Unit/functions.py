f15b9639678ee7ac391bc251311184802e3a3b46
Tsivilko Yaroslav <y.tsivilko@room8studio.com>
2018-06-22 18:23:44 +0300

    New line

--- learn_python/Unit/functions.py
+++ learn_python/Unit/functions.py
@@ -319,3 +319,85 @@ unprinted_designs = ['iphone case', 'robot pendant', 'dodecahedron']
 completed_models = []
 print_models(unprinted_designs, completed_models)
 show_completed_models(completed_models)
+
+#########################################################################################
+
+# TIY
+"""8-9. Magicians: Make a list of magician’s names. Pass the list to a function
+called show_magicians(), which prints the name of each magician in the list."""
+def show_magicians(magicians_name):
+    "Show each magician name"
+    for magician in magicians:
+        print ("Hello, " + magician.title())
+
+
+magicians = ['Bob', 'Carl', 'Luis']
+show_magicians(magicians)
+
+"""8-10. Great Magicians: Start with a copy of your program from Exercise 8-9.
+Write a function called make_great() that modifies the list of magicians by adding
+the phrase the Great to each magician’s name. Call show_magicians() to
+see that the list has actually been modified."""
+def show_magicians(magicians):
+    """Print the name of each magician in the list."""
+    for magician in magicians:
+        print(magician)
+
+def make_great(magicians):
+    """Add 'the Great!' to each magician's name."""
+    # Build a new list to hold the great musicians.
+    great_magicians = []
+
+    # Make each magician great, and add it to great_magicians.
+    while magicians:
+        magician = magicians.pop()
+        great_magician = magician + ' the Great'
+        great_magicians.append(great_magician)
+
+    # Add the great magicians back into magicians.
+    for great_magician in great_magicians:
+        magicians.append(great_magician)
+
+magicians = ['Harry Houdini', 'David Blaine', 'Teller']
+show_magicians(magicians)
+
+print("\n")
+make_great(magicians)
+show_magicians(magicians)
+
+"""8-11. Unchanged Magicians: Start with your work from Exercise 8-10. Call the
+function make_great() with a copy of the list of magicians’ names. Because the
+original list will be unchanged, return the new list and store it in a separate list.
+Call show_magicians() with each list to show that you have one list of the original
+names and one list with the Great added to each magician’s name."""
+def show_magicians(magicians):
+    """Print the name of each magician in the list."""
+    for magician in magicians:
+        print(magician)
+
+def make_great(magicians):
+    """Add 'the Great!' to each magician's name."""
+    # Build a new list to hold the great musicians.
+    great_magicians = []
+
+    # Make each magician great, and add it to great_magicians.
+    while magicians:
+        magician = magicians.pop()
+        great_magician = magician + ' the Great'
+        great_magicians.append(great_magician)
+
+    # Add the great magicians back into magicians.
+    for great_magician in great_magicians:
+        magicians.append(great_magician)
+
+    return magicians
+
+magicians = ['Harry Houdini', 'David Blaine', 'Teller']
+show_magicians(magicians)
+
+print("\nGreat magicians:")
+great_magicians = make_great(magicians[:])
+show_magicians(great_magicians)
+
+print("\nOriginal magicians:")
+show_magicians(magicians)
